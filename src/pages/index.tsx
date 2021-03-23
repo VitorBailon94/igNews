d7f089d2d1faeefad50493deba0086bfa29a5c6a
Vitor Bailon <vitor.carulla@gmail.com>
2021-03-23 17:00:06 -0300

    Ajustando programas e Começando aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.News</title>
+      </Head>
+      <h1> Oi lindo</h1>
+    </>
+  )
+}
