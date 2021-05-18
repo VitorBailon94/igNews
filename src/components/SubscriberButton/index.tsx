6f3953b7e7058ed571b26aac557903b2f3c57763
Vitor Bailon <vitor.carulla@gmail.com>
2021-05-18 16:32:56 -0300

    atualização do projeto

--- src/components/SubscriberButton/index.tsx
+++ src/components/SubscriberButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscriberButtonProps {
@@ -5,8 +8,30 @@ interface SubscriberButtonProps {
 }
 
 export function SubscriberButton({ priceId }: SubscriberButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscriber() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscriber}>
       Subscriber Now
     </button>
   )
